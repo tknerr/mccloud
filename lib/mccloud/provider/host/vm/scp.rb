568d3b00d274cd4330f4c3e32cc6e080025d7c4b
tkn <tkn@zuehlke.com>
2012-08-21 17:14:16 +0200

    GH-24: pass @private_key_path and @port as ssh options to Net::SCP.upload

--- lib/mccloud/provider/host/vm/scp.rb
+++ lib/mccloud/provider/host/vm/scp.rb
@@ -7,10 +7,19 @@ module Mccloud::Provider
         scp(src,dest)
       end
 
-       def scp(src,dest)
-         Net::SCP.upload!(ip_address,@user,src,dest)
+      def scp(src,dest)
+        Net::SCP.upload!(ip_address,@user,src,dest, :ssh => ssh_options)
       end
 
-       end #module
-      end #module
+      # pass options as described here:
+      # http://net-ssh.github.com/ssh/v2/api/classes/Net/SSH.html#M000002
+      def ssh_options
+        ssh_opts = Hash.new
+        (ssh_opts[:keys] = @private_key_path) if @private_key_path
+        (ssh_opts[:port] = @port) if @port
+        ssh_opts
+      end
+
     end #module
+  end #module
+end #module
